44b8bd68eae5db8b377c2e0aa40c35edda794d1d
Bram Elfrink <bramelfrink@gmail.com>
2016-05-07 12:58:04 +0200

    Included the child theme

---
+++ Influence-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+function theme_enqueue_scripts() {
+    wp_enqueue_script( 'js-script', get_template_directory_uri() . '/js.css' );
+}
+?>
